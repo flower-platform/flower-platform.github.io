c54d876b25d73b1f14369d48d0b4e5ce49e8f751
Cristian Spiescu <cristian.spiescu@crispico.com>
2025-03-08 10:44:48 +0100

    TAD

---
+++ tests-are-demo/assets/featurebookUiRecorded-Bz6yLif3.js
@@ -0,0 +1,2 @@
+import{R as e,j as s,K as t,N as o}from"./semantic.min-CDEmuUzs.js";const{RECORDED_TESTS_DIR:r,RECORDED_TESTS_INDEX:d}=o;e.render(s.jsx(t,{recordedTestsUrl:"http://localhost:3000/"+r+d,selectedTestClass:"TodosTestsAreDemo",includeCredentialsInJsonRequestRegex:/https:\/\/gitlab\.xops-online\.com\/*/}),document.getElementById("root"));
+//# sourceMappingURL=featurebookUiRecorded-Bz6yLif3.js.map
